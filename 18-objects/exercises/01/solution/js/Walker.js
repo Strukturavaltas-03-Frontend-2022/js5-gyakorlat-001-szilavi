38e0c1ecc2a7e6139ab462dd8926189416928724
k3csk3 <szilagyi.viktor89@gmail.com>
2022-09-20 08:50:19 +0200

    lefut

--- 18-objects/exercises/01/solution/js/Walker.js
+++ 18-objects/exercises/01/solution/js/Walker.js
@@ -1,1 +1,37 @@
+function Walker(name = "John Doe", state = "dead") {
+  this.name = name;
+  this.state = state;
+
+  Walker.prototype.walk = function (speedInMph = 10) {
+    return `${this.name} the ${this.state} walks with ${speedInMph} mph!`;
+  };
+  Walker.prototype.eat = function (food = "meat") {
+    return `${this.name} the ${this.state} eats ${food}!`;
+  };
+  Walker.prototype.speak = function (sound = "grrrrrr") {
+    return `${this.name} the ${this.state} says ${sound}!`;
+  };
+  Object.defineProperty(this, "firstName", {
+    enumerable: true,
+    configurable: true,
+    get() {
+      return this.name.split(" ")[0];
+    },
+    set(value) {
+      this.name = `${value} ${this.name.split(" ")[1]}`;
+    },
+  });
+
+  Object.defineProperty(this, "lastName", {
+    enumerable: true,
+    configurable: true,
+    get() {
+      return this.name.split(" ")[1];
+    },
+    set(value) {
+      this.name = `${this.name.split(" ")[0]} ${value}`;
+    },
+  });
+}
+
 export default Walker;
